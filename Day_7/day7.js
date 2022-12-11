de49d2e57e878e0f6c64488439259e4a53a0aeec
Jonatan Garbuyo <jonatangarbuyo@gmail.com>
2022-12-11 13:13:51 -0300

    refactor code

--- Day_7/day7.js
+++ Day_7/day7.js
@@ -8,78 +8,69 @@ try {
   const [totalRootDir, totalCount, allDirectories] = getSumDirectoriesSizes(
     filesystem['/']
   )
-  const dirToDelete = getDirToDelete(allDirectories, totalRootDir)
+  const sizeOfDirToDelete = getSizeOfDirToDelete(allDirectories, totalRootDir)
 
   console.log('totalSumOfSmallDirectories: ', totalCount)
-  console.log('dirToDelete: ', dirToDelete)
+  console.log('dirToDelete: ', sizeOfDirToDelete)
 } catch (err) {
   console.log(err)
 }
 
 // Helper Functions
-function getDirToDelete(allDirectories, totalRootDir) {
-  const totalDiskSpace = 70000000
-  const neededSpace = 30000000
-  let freeSpace = totalDiskSpace - totalRootDir
-  console.log('freeSpace: ', totalDiskSpace - totalRootDir)
+function getSizeOfDirToDelete(allDirectories, totalRootDir) {
+  const TOTAL_DISK_SPACE = 70000000
+  const NEEDED_SPACE = 30000000
+  const freeSpace = TOTAL_DISK_SPACE - totalRootDir
 
   const directories = allDirectories.sort((dirA, dirB) => dirA - dirB)
   for (let index = 0; index < directories.length; index++) {
     const dir = directories[index]
-    if (freeSpace + dir >= neededSpace) return dir
+    if (freeSpace + dir >= NEEDED_SPACE) return dir
   }
 }
 
 function getSumDirectoriesSizes(directories) {
   let totalRootDir = 0
   let totalCount = 0
-  const directoriesToDelete = []
+  const allDirectories = []
 
   function getSumDirSize(dir) {
     if (!dir.hasOwnProperty('size')) return 0
 
     let count = 0
     for (const [key, value] of Object.entries(dir)) {
-      if (key === 'size') {
-        count += value
-        continue
-      }
-      if (key === 'parentDir' || typeof value !== 'object') continue
+      if (key === 'parentDir') continue
       const size = getSumDirSize(value)
-      count += size
+      count += key === 'size' ? value : size
     }
 
     if (count <= 100_000) totalCount += count
-    if (count > 0) directoriesToDelete.push(count)
+    if (count > 0) allDirectories.push(count)
 
     return count
   }
 
   for (const [key, value] of Object.entries(directories)) {
-    if (key === 'size') {
-      totalRootDir += value
-      continue
-    }
-    if (key === 'parentDir' || typeof value !== 'object') continue
+    if (key === 'parentDir') continue
     const size = getSumDirSize(value)
-    totalRootDir += size
+    totalRootDir += key === 'size' ? value : size
   }
 
-  return [totalRootDir, totalCount, directoriesToDelete]
+  return [totalRootDir, totalCount, allDirectories]
 }
 
 function parseFilesystem(terminalOutputList) {
-  const sizeInit = 0
-  const filesystem = {}
-  let currentDir = filesystem
+  const INITIAL_SIZE = 0
+  const FILESYSTEM = {}
+  let currentDir = FILESYSTEM
 
-  const addNewFile = (newfile, fileSize) => {
-    // currentDir[newfile] = fileSize
+  const addNewFile = (fileSize) => {
     currentDir.size += fileSize
   }
 
   const initDirectory = (dir) => {
-    if (!currentDir.hasOwnProperty(dir)) currentDir[dir] = { size: sizeInit }
+    if (!currentDir.hasOwnProperty(dir))
+      currentDir[dir] = { size: INITIAL_SIZE }
   }
 
   const changeDirectory = (cdArg) => {
@@ -99,9 +90,9 @@ function parseFilesystem(terminalOutputList) {
     } else if (rowList[0] === 'dir') {
       initDirectory(rowList[1])
     } else if (!isNaN(rowList[0])) {
-      addNewFile(rowList[1], parseInt(rowList[0]))
+      addNewFile(parseInt(rowList[0]))
     }
   })
 
-  return filesystem
+  return FILESYSTEM
 }
